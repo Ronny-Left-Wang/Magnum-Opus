c1e1b4be393afd2a0783f05d81aeba7ba5e7df80
yinghaoawang <yinghao.a.wang@gmail.com>
2019-07-19 15:57:51 -0700

    reads post requests to move guy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const size = 30;
 let world;
@@ -16,28 +20,61 @@ let createWorld = (size) => {
     return world;
 }
 
+let guy = {
+    x: 0,
+    y: 0
+}
+
 let displayWorld = (world) => {
     let res = "";
     for (let i = 0; i < world.length; ++i) {
         for (let j = 0; j < world[i].length; ++j) {
-            res += `[${world[i][j]}]`;
+            let display = world[i][j];
+            if (guy.x == j && guy.y == i) display = 'G';
+            res += `[${display}]`;
         }
         res += '<br />';
     }
     return res;
 }
 
+/*
 let interval = setInterval(() => {
     let randI = Math.floor(Math.random() * size);
     let randJ = Math.floor(Math.random() * size);
     let randL = String.fromCharCode("A".charCodeAt(0) + Math.floor(Math.random() * 24));
     world[randI][randJ] = randL;
+    guy.y++;
+
 }, 1000);
+*/
 
 world = createWorld(size);
 
 app.get('/', (req, res) => {
     res.send(displayWorld(world));
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    let action = req.body.action;
+    switch (action) {
+        case 'up':
+            guy.y--;
+            break;
+        case 'down':
+            guy.y++;
+            break;
+        case 'left':
+            guy.x--;
+            break;
+        case 'right':
+            guy.x++;
+            break;
+        default:
+            break;
+    }
+    res.send('huh');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
