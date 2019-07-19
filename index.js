9abed05bbbd2095c76564a4e09cd31131bba7f63
yinghaoawang <yinghao.a.wang@gmail.com>
2019-07-19 16:34:32 -0700

    added instructions for ronny assignment

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ let displayWorld = (world) => {
     let res = "";
     for (let i = 0; i < world.length; ++i) {
         for (let j = 0; j < world[i].length; ++j) {
+            // TODO DISPLAY GOOD
             let display = world[i][j];
             if (guy.x == j && guy.y == i) display = 'G';
             res += `[${display}]`;
@@ -51,13 +52,22 @@ let interval = setInterval(() => {
 
 let moveButtonHtml = `
     <h1>YAYO YAYOOOOO</h1>
-    <form action="/" method="post">
+    <!-- TODO Do in button form instead --> <form action="/" method="post">
         <input type="radio" name="action" value="up" checked> Up<br>
         <input type="radio" name="action" value="down"> Down<br>
         <input type="radio" name="action" value="left"> Left<br>
         <input type="radio" name="action" value="right"> Right<br>
         <input type="submit" value="I am but a submit">
     </form>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        // javascript
+        /* TODO USE BUTTONS TO MOVE CHARACTER
+        if dude press WASD then
+            // use $.ajax
+            send a post request to / to move the niga
+        */
+    </script>
 `;
 
 world = createWorld(size);
