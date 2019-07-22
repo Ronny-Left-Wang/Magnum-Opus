1d9fefecf459951e4c9637631228e9168a1018f9
Ronnapob Ritprasert <Ronnyrit@gmail.com>
2019-07-21 18:13:11 -0700

    Added blue and green tiles

--- canvas.js
+++ canvas.js
@@ -7,8 +7,34 @@ let container = `
         let ctx = c.getContext('2d');
         let width = c.width;
         let height = c.height;
-        let x = 20;
-        let y = 20;
+        let x = 0;
+        let y = 0;
+
+        // Fills tiles with green
+        while (y != width) {
+            let chance = Math.random();
+            if (chance > 0.20) {
+                ctx.fillStyle = "#00FF00";
+            } else {
+                ctx.fillStyle = "#0066FF";
+            }
+            ctx.fillRect(x, y, 20, 20);
+            x += 20;
+            if (x == width) {
+                y += 20;
+                x = 0;
+            }
+        }
+
+        // Wow i found 2 ways to do this. lol
+        // ctx.fillStyle = "#00FF00";
+        // ctx.fillRect(0, 0, width, height);
+        
+        // x y reset
+        y = 20;
+        x = 20;
+
+        // Draws grid pattern.
         while (y != width) {
             ctx.beginPath();
             ctx.moveTo(x, 0);
@@ -21,8 +47,6 @@ let container = `
             ctx.stroke();
             y += 20;
         }
-    
-    
     </script>
 
 
