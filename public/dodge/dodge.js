cc8d1e0fa1df61438f9c23ac8a7d7cc248fa52ba
Ronnapob Ritprasert <Ronnyrit@gmail.com>
2019-08-12 21:02:31 -0700

    Added player idle and move color, target movment..

--- public/dodge/dodge.js
+++ public/dodge/dodge.js
@@ -1,25 +1,11 @@
-class Player {
-    constructor(name, size, position) {
-        this.name = name;
-        this.size = size;
-        this.position = position;
-        this.target = null;
-    }
+const PLAYER_IDLE_COLOR = { r: 0, g: 0, b: 55 };
+const PLAYER_MOVE_COLOR = { r: 55, g: 100, b: 50 };
 
-    display() {
-        fill(255, 0, 0);
-        ellipse(this.position.x, this.position.y, 20, 20);
-    }
-
-    step() {
-        if (this.target) {
-            moveTowardTarget();
-        }
-    }
-
-    moveTowardTarget() {
+class Enemy {
+    constructor() {
 
     }
+
 }
 
 let ronpob;
@@ -28,14 +14,69 @@ function setup() {
     let canvas = createCanvas(800, 600);
     ronpob = new Player('ronpob', 20, { x: 10, y: 20 });
 }
-let x = 0;
 
 function draw() {
     background(51);
-    ellipse(x, 0, 100, 100);
-    x++;
-    ronpob.position.x = mouseX;
-    ronpob.position.y = mouseY;
+    if (mouseIsPressed && mouseX <= width && mouseY <= width && mouseX >= 0 && mouseY >= 0) {
+        ronpob.target = { x: mouseX, y: mouseY };
+    }
+    ronpob.step();
     ronpob.display();
 }
 
+function fillColor(colorObject) {
+    fill(colorObject.r, colorObject.g, colorObject.b);
+}
+
+class Player {
+    constructor(name, size, position) {
+        this.name = name;
+        this.size = size;
+        this.position = position;
+        this.speed = 5;
+        this.target = null;
+    }
+
+    display() {
+        if (this.target == null) {
+            fillColor(PLAYER_IDLE_COLOR);
+        } else {
+            fillColor(PLAYER_MOVE_COLOR);
+        }
+        ellipse(this.position.x, this.position.y, 20, 20);
+    }
+
+    step() {
+        if (this.target) {
+            this.moveTowardTarget();
+        }
+    }
+
+    moveTowardTarget() {
+        let tx = this.target.x - this.position.x,
+            ty = this.target.y - this.position.y,
+            dist = Math.sqrt(tx * tx + ty * ty);
+
+        if (dist == 0) {
+            this.target = null;
+            return;
+        }
+
+        let postPosition = {
+            x: this.position.x + (tx / dist) * this.speed,
+            y: this.position.y + (ty / dist) * this.speed
+        };
+
+        let px = postPosition.x - this.position.x,
+            py = postPosition.y - this.position.y;
+        let postDist = Math.sqrt(px * px + py * py);
+
+        if (postDist > dist) {
+            this.position = this.target;
+            this.target = null;
+            return;
+        } else {
+            this.position = postPosition;
+        }
+    }
+}
